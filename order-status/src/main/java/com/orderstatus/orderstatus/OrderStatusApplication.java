792a2f054ec1b853a2c67c4daa1fd968ed72bc54
ashley <ashleykubera@gmail.com>
2020-11-16 13:51:26 -0500

    first commit

---
+++ order-status/src/main/java/com/orderstatus/orderstatus/OrderStatusApplication.java
@@ -0,0 +1,13 @@
+package com.orderstatus.orderstatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderStatusApplication.class, args);
+	}
+
+}
---
+++ order-status/src/test/java/com/orderstatus/orderstatus/OrderStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderstatus.orderstatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
